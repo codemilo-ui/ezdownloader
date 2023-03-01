e6a9bf28ed17eed69fd18150f6f68ede3df59445
codemilo <miloismypetdog@gmail.com>
2023-03-01 17:19:22 +0530

    Fix

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup
 setup(
     name='ezdownloader',
     version='0.1.0',
+    description = 'A simple file downloading tool ',
+    author = 'codemilo',
+    author_email = 'milomurthy@gmail.com',
+    url = 'https://github.com/codemilo-ui/ezdownloader',
     packages=['ezdownloader'],
+    keywords = ['tag1', 'tag2'],
     install_requires=[
         'urllib3',
         'tqdm'
