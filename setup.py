17745cf7af01482aa0a1e8c69e84442eaaac6bf0
codemilo <miloismypetdog@gmail.com>
2023-03-02 06:37:23 +0530

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='ezdownloader',
-    version='0.1.0',
+    version='0.1.1',
     description = 'A simple file downloading tool ',
     author = 'codemilo',
     author_email = 'milomurthy@gmail.com',
     url = 'https://github.com/codemilo-ui/ezdownloader',
     packages=['ezdownloader'],
-    keywords = ['tag1', 'tag2'],
+    keywords = ['downloader', 'wget'],
     install_requires=[
         'urllib3',
         'tqdm'
